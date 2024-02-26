3ee4e96384c03c4763470f4a09f012b2025a323f
dineshdhakar <dhakardinesh1997@gmail.com>
2024-02-26 20:50:11 +0530

    update

---
+++ git_commit.py
@@ -0,0 +1,6 @@
+import os
+os.system('cd /Users/dineshdhakar/Desktop/codepython/urlShort/')
+os.system('ls')
+os.system('git add .')
+os.system('git commit -m "update"')
+os.system('git push -u origin main')
