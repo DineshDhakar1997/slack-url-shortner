0ed6b09aad8dd5fb2ed13e222c8d2ab387e1518d
dineshdhakar <dhakardinesh1997@gmail.com>
2024-02-27 12:03:22 +0530

    update

--- shortner/views.py
+++ shortner/views.py
@@ -1,6 +1,8 @@
 from .models import ShortUrlModel,generate_short_code
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import redirect
+
 
 class ShortenUrl(APIView):
     def post(self, request):
@@ -10,17 +12,25 @@ class ShortenUrl(APIView):
         try:
             short_url = ShortUrlModel.objects.get(original_url=original_url)
             short_code = short_url.short_code
-            return Response({'short_code': short_code})
+            return Response({'short_url': "http://127.0.0.1:8000/redir/"+short_code})
         except ShortUrlModel.DoesNotExist:
             short_code = generate_short_code(original_url)
             short_url = ShortUrlModel.objects.create(original_url=original_url,short_code=short_code)
             return Response({'short_code': short_code})
         
+# class RedirectToUrl(APIView):
+#     def get(self, request, short_code):
+#         try:
+#             short_url = ShortUrlModel.objects.get(short_code=short_code)
+#             return Response({'original_url': short_url.original_url}, status=302) #302 will redirect to original url
+#         except ShortUrlModel.DoesNotExist:
+#             return Response({'error': 'Invalid short code'}, status=404)
 class RedirectToUrl(APIView):
     def get(self, request, short_code):
         try:
             short_url = ShortUrlModel.objects.get(short_code=short_code)
-            return Response({'original_url': short_url.original_url}, status=302) #302 will redirect to original url
+            original_url = short_url.original_url
+            return redirect(original_url)  # Redirect to the original URL
         except ShortUrlModel.DoesNotExist:
             return Response({'error': 'Invalid short code'}, status=404)
 
