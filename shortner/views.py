8555f3626254d9f73f8f4d4b2345c910bc6228e5
dineshdhakar <dhakardinesh1997@gmail.com>
2024-03-02 02:02:34 +0530

    updated requirments

--- shortner/views.py
+++ shortner/views.py
@@ -10,7 +10,7 @@ import requests
 
 logger = logging.getLogger(__name__)
 client = slack_sdk.WebClient(
-    token="xoxb-6721483596338-6731107941204-hHuZbjq3M4L8ZWCHpt5DXvV8"
+    token="xoxb-6721483596338-6731107941204-bq0lJm1GJiJTtxtQvxk2b3i6"
 )
 BOT_ID = client.api_call("auth.test")["user_id"]
 
@@ -45,13 +45,8 @@ class ShortenUrl(APIView):
 class RedirectToUrl(APIView):
     def get(self, request, short_code):
         try:
-
             short_url = ShortUrlModel.objects.get(short_code=short_code)
-
             original_url = short_url.original_url
-            # return Response({'original_url':original_url}, status=200)
-
-            # return redirect(original_url)
             return redirect(original_url)  # Redirect to the original URL
         except ShortUrlModel.DoesNotExist:
             return Response({"error": "Invalid short code"}, status=404)
@@ -59,12 +54,7 @@ class RedirectToUrl(APIView):
 
 class SlackEventHandler(APIView):
     def post(self, request):
-        payload = request.body.decode("utf-8")
-
         data = request.data
-        json_load = json.dumps(data)
-        with open("url.txt", "a") as f:
-            f.write(json_load + "\n")
         if data.get("type") == "url_verification":
             return Response({"challenge": data.get("challenge")})
 
@@ -74,13 +64,21 @@ class SlackEventHandler(APIView):
                 message = message[1:]
             if message[-1] is not None:
                 message = message[:-1]
-
+            original_url = message
             user = data.get("event").get("user")
-            shortened_url = call_shorten_url(message)
+            try:
+                short_url = ShortUrlModel.objects.get(original_url=original_url)
+                short_code = short_url.short_code
+                short_url= "https://slackurlshortner-fd74f723a2da.herokuapp.com/redir/"+ short_code
+            except ShortUrlModel.DoesNotExist:
+                short_code = generate_short_code(original_url)
+                short_url = ShortUrlModel.objects.create(
+                original_url=original_url, short_code=short_code)
+            # shortened_url = call_shorten_url(message)
 
             if BOT_ID != user:
-                client.chat_postMessage(channel="#shortner", text=shortened_url)
-        return Response({"shortened_url": data.get("event").get("text")}, status=200)
+                client.chat_postMessage(channel="#shortner", text=short_url)
+        return Response({"shortened_url": short_url}, status=200)
 
 
 def call_shorten_url(url):
