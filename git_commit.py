3665c5a986cead7550395f07634eb23ae088032d
dineshdhakar <dhakardinesh1997@gmail.com>
2024-03-02 02:36:15 +0530

    moved secret key to .env

--- git_commit.py
+++ git_commit.py
@@ -2,5 +2,5 @@ import os
 os.chdir('/Users/dineshdhakar/Desktop/codepython/urlShort/')
 os.system('ls')
 os.system('git add .')
-os.system('git commit -m "removed secret key from "')
+os.system('git commit -m "moved secret key to .env"')
 os.system('git push -u origin main')
